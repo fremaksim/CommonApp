402b1f3f3da27780f047591a6424b81e2eb87924
fremaksim <lxwgh2007@gmail.com>
2019-01-21 11:45:58 +0800

    string extension

---
+++ CommonProject/CommonProject/Components-9-组件（不属于这里采用CocoaPods集成）/BasicComponents/String/String+Slice.swift
@@ -0,0 +1,15 @@
+//
+//  String+Slice.swift
+//  CommonProject
+//
+//  Created by mozhe on 2019/1/21.
+//  Copyright © 2019 mozhe. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: - 字符串截取
+extension String {
+
+}
--- CommonProject/CommonProject/ViewController.swift
+++ CommonProject/CommonProject/ViewController.swift
@@ -13,8 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let strings = ["文","戊戌","百日维新aa","远征队113","毒液9090wq",""]
+        
+        for str in strings {
+            if String.isAvailableLength(str: str, miniLength: 4, maxlength: 10) == true {
+                print(str,"true", separator: ",", terminator: ".")
+            }else {
+                print(str,"false", separator: ",", terminator: ".")
+            }
+            print(str.count)
     }
+        
 
-
+    }
 }
 
